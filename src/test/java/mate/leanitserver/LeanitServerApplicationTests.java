849524410d1484fa7a659dd79d5996c4b00b65ea
Bakhmet Ivan <bahmetvanya1290@gmail.com>
2024-06-29 14:32:30 +0300

    Start of the project

---
+++ src/test/java/mate/leanitserver/LeanitServerApplicationTests.java
@@ -0,0 +1,13 @@
+package mate.leanitserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeanitServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
